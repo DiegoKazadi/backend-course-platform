6a4251251bb9ea679fde669f12789baccd0c0f79
DiegoKazadi <diegokazadi@gmail.com>
2026-04-06 21:44:35 -0300

    chore: generate project using Spring Initializr

---
+++ courseplatform/src/test/java/com/diego/courseplatform/CourseplatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diego.courseplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseplatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
