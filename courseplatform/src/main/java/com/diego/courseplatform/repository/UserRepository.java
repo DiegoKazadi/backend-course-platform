c38d4f7866006c559d9da4f9d5efb80cf18a130c
DiegoKazadi <diegokazadi@gmail.com>
2026-04-08 21:20:28 -0300

    feat: create User entity and fix repository structure

---
+++ courseplatform/src/main/java/com/diego/courseplatform/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.diego.courseplatform.repository;
+
+import com.diego.courseplatform.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+}
